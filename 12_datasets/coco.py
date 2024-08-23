a66ef566002be870b751208dbe1b01f5a2237574
Farrukh Jadoon <fkjadoon94@gmail.com>
2024-08-23 00:13:08 -0400

    Update coco.py

--- 12_datasets/coco.py
+++ 12_datasets/coco.py
@@ -9,6 +9,7 @@
 # It is recommended to iterate on this code from a modal.Function running Jupyter server.
 # This better supports experimentation and maintains state in the face of errors:
 # 11_notebooks/jupyter_inside_modal.py
+
 import os
 import pathlib
 import shutil
